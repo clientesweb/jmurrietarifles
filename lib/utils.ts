e336f4d0b63ce29ac9559fe77a5bb70bc4566a75
clientesweb <alexisifran32@gmail.com>
2025-04-18 14:54:07 -0300

    Actualizar utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+
+export const formatPrice = (amount: number) => {
+  return amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")
+}
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function formatPrice(price: number): string {
-  return price.toLocaleString("es-AR")
 }
